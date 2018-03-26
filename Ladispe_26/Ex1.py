2ab2c9244f7b2c8ed7b99ed50240841e444d6e69
MartinaCane <s227716@studenti.polito.it>
2018-03-26 15:04:23 +0200

    Ex 1

---
+++ Ladispe_26/Ex1.py
@@ -0,0 +1,33 @@
+print("Insert the number corresponding to the action you want to perform:")
+print("1. insert a new task; 2. remove a task; 3. show all the tasks, sorted in alphabetic order; 4. close the program.")
+print("Your choice: ")
+choice=input()
+
+while choice!='4':
+    if choice=='1':
+        ask= input("Go on: ")
+        tasks=[ask]
+        choice2=input("You want to add more? (Y/N) ")
+        while choice2=='Y':
+            tasks.append(input("Go on: "))
+            print("Your list is: ", tasks)
+            choice2=input("You want to add more? (Y/N) ")
+        if choice2=='N':
+             choice=input("What now? ")
+
+    if choice=='2':
+        tasks.remove(input("Insert a task to be removed: "))
+        print(tasks, " Task successfully delated ")
+        choice3=input("You want to delate more? (Y/N) ")
+        while choice3=='Y':
+            tasks.remove(input("Go on: "))
+            print(tasks, " Task successfully delated ")
+            choice3=input("You want to delate more? (Y/N) ")
+        if choice3=='N':
+             choice=input("What now? ")
+
+    if choice=='3':
+        print(sorted(tasks))
+        choice=input("What now? ")
+
+print("Bye")
