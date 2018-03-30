3025782a8da3eca1d8906a55acd2d3af95286205
MartinaCane <Matrix9697>
2018-03-31 01:12:08 +0200

    Ex 4

--- Ladispe_26/Ex4.py
+++ Ladispe_26/Ex4.py
@@ -1,1 +1,21 @@
+task1 = {"todo": "call John for AmI project organization", "urgent": True}
+task2 = {"todo": "buy a new mouse", "urgent": True}
+task3 = {"todo": "find a present for Angelina's birthday", "urgent": False}
+task4 = {"todo": "organize mega party (last week of April)", "urgent": False}
+task5 = {"todo": "book summer holidays", "urgent": False}
+task6 = {"todo": "whatsapp Mary for a coffee", "urgent": False}
 
+
+
+if task1["urgent"]== True:
+    print(task1)
+if task2["urgent"]== True:
+    print(task2)
+if task3["urgent"]== True:
+    print(task3)
+if task4["urgent"]== True:
+    print(task4)
+if task5["urgent"]== True:
+    print(task5)
+if task6["urgent"]== True:
+    print(task6)
